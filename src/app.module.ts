0455042afa40dd5c016a1480fcd9e1c91e0f9f7c
Yonas Mekonnen <myonas886@gmail.com>
2025-01-29 10:05:06 +0300

    Refactor MongoDB connection handling to use Logger for connection events

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConnectionService } from './config/mongodb-connection.service';
+import { Connection } from 'mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017', {
       dbName: 'NestJs-Blog',
-      connectionFactory: (connection) => {
-        connection.on('connected', () => {
-          console.log('MongoDB connected');
-        });
-        connection.on('error', (error: any) => {
-          console.log('MongoDB connection error:', error);
-        });
-        connection._events.connected();
+      onConnectionCreate: (connection: Connection) => {
+        connection.on('connected', () => Logger.log('MongoDB connected'));
+        connection.on('disconnected', () => Logger.log('disconnected'));
+        connection.on('reconnected', () => Logger.log('reconnected'));
         return connection;
       },
     }),
