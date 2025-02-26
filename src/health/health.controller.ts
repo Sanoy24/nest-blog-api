bf205f65a349ca88d67768067aabe8166b5dead2
Yonas Mekonnen <myonas886@gmail.com>
2025-02-26 08:49:43 +0300

    healthcheck fix

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -3,24 +3,29 @@ import {
   HealthCheckService,
   HttpHealthIndicator,
   HealthCheck,
-  MongooseHealthIndicator,
+  MongooseHealthIndicator, // Import the correct indicator
 } from '@nestjs/terminus';
-import { Public } from 'src/shared/utils/publicRoute';
-@Public()
+import { Public } from 'src/shared/utils/publicRoute'; // If applicable
+
+@Public() // If applicable
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
-    private database: MongooseHealthIndicator,
+    private db: MongooseHealthIndicator, // Use MongooseHealthIndicator
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([
-      () => this.http.pingCheck('nest-js', 'http://localhost:3001/'),
-      () => this.database.pingCheck('database'),
+      () =>
+        this.http.pingCheck(
+          'server',
+          'https://nest-blog-api-2ael.onrender.com',
+        ),
+      () => this.db.pingCheck('database'), // Use the pingCheck method
     ]);
   }
 }
