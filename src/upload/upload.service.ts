b9e7382cc0aba9cc957043b60bca0f33ba8634ff
Yonas Mekonnen <myonas886@gmail.com>
2025-02-28 11:08:28 +0300

    file naming updated for uploading images

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import { Readable } from 'stream';
 
 @Injectable()
 export class UploadService {
   async uploadImage(file: Express.Multer.File): Promise<string> {
+    let filename = file.originalname;
+
+    if (file.originalname.split(' ') !== undefined) {
+      filename = file.originalname.split(' ').join('-').split('.')[0];
+    }
+
     return new Promise((resolve, reject) => {
-      v2.uploader
-        .upload_stream(
-          { folder: 'blog_posts' },
-          (err: UploadApiErrorResponse, result: UploadApiResponse) => {
-            if (err) return reject(new Error(err.message));
-            resolve(result.secure_url);
-          },
-        )
-        .end(file.buffer);
+      const stream = v2.uploader.upload_stream(
+        {
+          folder: 'blog_posts',
+          public_id: `${filename}-${Date.now()}`,
+          overwrite: true,
+        },
+        (err: UploadApiErrorResponse, result: UploadApiResponse) => {
+          if (err) return reject(new Error(err.message));
+          resolve(result.secure_url);
+        },
+      );
+      Readable.from(file.buffer).pipe(stream);
     });
   }
 }
